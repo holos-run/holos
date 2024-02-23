bdd76c78a72bf8e7b289ab8f89b8b377394b8a44
Jeff McCune <jeff@openinfrastructure.co>
2024-02-22 21:42:07 -0800

    Refactor txtar package for readability

--- pkg/cli/txtar/txtar.go
+++ pkg/cli/txtar/txtar.go
@@ -10,6 +10,7 @@ import (
 	"golang.org/x/tools/txtar"
 	"io"
 	"io/fs"
+	"log/slog"
 	"os"
 	"path/filepath"
 )
@@ -20,16 +21,16 @@ func New(cfg *config.Config) *cobra.Command {
 	cmd.Short = "trivial text-based file archives"
 	cmd.Long = "writes arguments to stdout otherwise extracts"
 	cmd.Args = cobra.MinimumNArgs(0)
-	cmd.RunE = makeTxtarRun(cfg)
+	cmd.RunE = makeRunFunc(cfg)
 	cmd.Flags().SortFlags = false
 	cmd.Flags().AddGoFlagSet(cfg.TxtarFlagSet())
 	return cmd
 }
 
-func makeTxtarRun(cfg *config.Config) command.RunFunc {
+func makeRunFunc(cfg *config.Config) command.RunFunc {
 	return func(cmd *cobra.Command, args []string) error {
 		if len(args) == 0 {
-			return txExtract(cfg)
+			return extract(cfg)
 		}
 		a := &txtar.Archive{}
 		for _, name := range args {
@@ -49,7 +50,7 @@ func makeWalkFunc(a *txtar.Archive) fs.WalkDirFunc {
 		}
 
 		if !d.IsDir() {
-			if file, err := txFile(path); err != nil {
+			if file, err := file(path); err != nil {
 				return wrapper.Wrap(err)
 			} else {
 				a.Files = append(a.Files, file)
@@ -60,46 +61,55 @@ func makeWalkFunc(a *txtar.Archive) fs.WalkDirFunc {
 	}
 }
 
-func txFile(path string) (file txtar.File, err error) {
+func file(path string) (file txtar.File, err error) {
 	file.Name = path
 	file.Data, err = os.ReadFile(path)
 	return
 }
 
-func txExtract(cfg *config.Config) error {
+// extract files from the configured Stdin to Stdout or the filesystem.
+func extract(cfg *config.Config) error {
 	input, err := io.ReadAll(cfg.Stdin())
 	if err != nil {
 		return wrapper.Wrap(fmt.Errorf("could not read stdin: %w", err))
 	}
 	archive := txtar.Parse(input)
-	header := bytes.Split(archive.Comment, []byte{'\n'})[:1]
-	if len(header) == 0 {
-		header = append(header, []byte{})
+	if idx := cfg.TxtarIndex(); idx != 0 {
+		return printFile(cfg.Stdout(), idx, archive)
 	}
 
-	// Print one file to stdout
-	idx := cfg.TxtarIndex()
+	return writeFiles(cfg.Logger(), archive)
+}
+
+// printFile prints one file from the txtar archive by index.
+func printFile(w io.Writer, idx int, a *txtar.Archive) (err error) {
+	if idx == 0 {
+		return wrapper.Wrap(fmt.Errorf("idx cannot be 0"))
+	}
 	if idx > 0 {
-		cfg.Write(command.EnsureNewline(archive.Files[idx-1].Data))
-		return nil
+		_, err = w.Write(command.EnsureNewline(a.Files[idx-1].Data))
+	} else {
+		_, err = w.Write(command.EnsureNewline(a.Files[len(a.Files)+idx].Data))
 	}
-	if idx < 0 {
-		tail := len(archive.Files)
-		cfg.Write(command.EnsureNewline(archive.Files[tail+idx].Data))
-		return nil
-	}
+	return
+}
 
-	// Write all files
-	for _, file := range archive.Files {
-		log := cfg.Logger().With("header", string(header[0]), "path", file.Name, "bytes", len(file.Data))
+// writeFiles writes all files in the archive.
+func writeFiles(logger *slog.Logger, a *txtar.Archive) (err error) {
+	var header string
+	if h := bytes.Split(a.Comment, []byte{'\n'})[:1]; len(h) > 0 {
+		header = string(h[0])
+	}
+	for _, file := range a.Files {
+		log := logger.With("header", header, "path", file.Name, "bytes", len(file.Data))
 		path := filepath.Join(".", file.Name)
 		log.Info("writing: " + file.Name)
-		if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		if err = os.MkdirAll(filepath.Dir(path), 0755); err != nil {
 			return wrapper.Wrap(fmt.Errorf("could not make directory: %w", err))
 		}
-		if err := os.WriteFile(path, file.Data, 0644); err != nil {
+		if err = os.WriteFile(path, file.Data, 0644); err != nil {
 			return wrapper.Wrap(fmt.Errorf("could not write file: %w", err))
 		}
 	}
-	return nil
+	return
 }
