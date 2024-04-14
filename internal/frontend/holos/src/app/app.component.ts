5dc5c6fbdf23cc460820fb5ce8306d6098396329
Jeff McCune <jeff@openinfrastructure.co>
2024-04-14 07:03:45 -0700

    (#127) ng add @angular/material
    And start working on the sidenav and toolbar.

--- internal/frontend/holos/src/app/app.component.ts
+++ internal/frontend/holos/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
