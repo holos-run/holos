590e6b556c1c47f33f1f940073e2d7fdd82e4311
Jeff McCune <jeff@openinfrastructure.co>
2024-04-15 10:43:24 -0700

    (#127) Generate Angular Material navigation
    Instead of trying to hand-craft a navigation sidebar and toolbar from
    Youtube videos, use the [navigation schematic][1] to quickly get a "good
    enough" UI.
    
        ng generate @angular/material:navigation nav
    
    [1]: https://material.angular.io/guide/schematics#navigation-schematic

--- internal/frontend/holos/src/app/app.component.ts
+++ internal/frontend/holos/src/app/app.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    MatToolbarModule,
-    MatIconModule,
-    MatSidenavModule,
-  ],
+  imports: [NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'holos';
+  title = 'Holos';
 }
