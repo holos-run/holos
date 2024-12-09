188ff95015b71edc897a2bdc1b98f795946b0018
Jeff McCune <jeff@openinfrastructure.co>
2024-12-08 19:31:18 -0800

    docs: enable youtube fullscreen
    Without this patch the fullscreen button is disabled.

--- doc/website/src/components/YouTube/index.tsx
+++ doc/website/src/components/YouTube/index.tsx
@@ -2,15 +2,15 @@ import styles from './styles.module.css';
 
 //Pulled from: https://gaudion.dev/blog/mdx-youtube-embed
 //components/mdx/YouTube.tsx
-export default function YouTube ({ id } : { id : string }){
-    return (
-      <div class={styles.videoWrapper}>
-        <iframe
-          className="aspect-video w-full"
-          src={"https://www.youtube.com/embed/" + id}
-          title="YouTube Video Player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        ></iframe>
-      </div>
-    );
-  };
+export default function YouTube({ id }: { id: string }) {
+  return (
+    <div class={styles.videoWrapper}>
+      <iframe
+        className="aspect-video w-full"
+        src={"https://www.youtube.com/embed/" + id}
+        title="YouTube Video Player"
+        allow="picture-in-picture; fullscreen; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;"
+      ></iframe>
+    </div>
+  );
+};
