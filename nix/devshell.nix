abb5b220379d46d3bb180208b46c1b05b54c9933
Cameron Smith <cameron.ray.smith@gmail.com>
2025-02-22 21:56:33 -0500

    feat(nix): add packages used in docs or adjacent
    Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- nix/devshell.nix
+++ nix/devshell.nix
@@ -3,10 +3,20 @@
   packages,
 }:
 pkgs.mkShell {
-  inputsFrom = [packages.default];
+  inputsFrom = [ packages.default ];
+  # see https://search.nixos.org/ for package name ids
   packages = with pkgs; [
+    cue
     go
     gopls
+    k3d
+    kind
+    kubectl
+    kubectx # includes kubens
+    k9s
+    kubernetes-helm
+    kustomize
+    timoni
     packages.default
   ];
 }
