1371e6a84b7cf3342fb35a3815081b5660a11039
Jeff McCune <jeff@openinfrastructure.co>
2025-04-03 15:50:32 -0700

    cmd: add holos version
    Always bugged me holos --version works but holos version does not.

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,15 +2,24 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
+	"os"
 
 	"github.com/holos-run/holos/internal/cli"
 	"github.com/holos-run/holos/internal/holos"
+	"github.com/holos-run/holos/version"
 )
 
 // MakeMain makes a main function for the cli or tests.
 func MakeMain(options ...holos.Option) func() int {
 	return func() (exitCode int) {
+		if len(os.Args) >= 2 && os.Args[1] == "version" {
+			if _, err := fmt.Println(version.GetVersion()); err != nil {
+				panic(err)
+			}
+			return 0
+		}
 		cfg := holos.New(options...)
 		slog.SetDefault(cfg.Logger())
 		ctx := context.Background()
